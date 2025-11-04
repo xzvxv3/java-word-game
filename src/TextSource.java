6761c3b27de184f36f364c35a7ba4dc5ababa8b0
xzvxv3 <xzvxv3@gmail.com>
2025-11-04 17:33:23 +0900

    영어 단어, 영한 단어, 사전 입력 추가

---
+++ src/TextSource.java
@@ -0,0 +1,41 @@
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.HashMap;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+import java.util.Vector;
+
+public class TextSource {
+
+    private Scanner sc;
+    private StringTokenizer st;
+    private Vector<String> wordVector = new Vector<>(1000); // 영어 단어만
+    private HashMap<String, String>  EngKorDict = new HashMap<>(); // 영,한 단어
+
+    public TextSource() {
+        try {
+            sc = new Scanner(new FileReader("resources/words/words_eng_1000.txt"));
+            while(sc.hasNext()) {
+                wordVector.add(sc.nextLine());
+            }
+
+            sc = new Scanner(new FileReader("resources/words/words_EngKorDic_800.txt"));
+            while(sc.hasNext()) {
+                st = new StringTokenizer(sc.nextLine(), "\t");
+                String eng = st.nextToken();
+                String kor = st.nextToken();
+                EngKorDict.put(eng, kor);
+            }
+
+        } catch(FileNotFoundException e) {
+            System.out.println("No File.");
+            System.exit(0);
+        }
+    }
+
+    public String getNextWord() {
+        int wordSize = wordVector.size();
+        int idx = (int) (Math.random() * wordSize);
+        return wordVector.get(idx);
+    }
+}
