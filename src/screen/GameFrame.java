d1e2c0c22ed2c1d0c94868637506362b7933dbc7
xzvxv3 <xzvxv3@gmail.com>
2025-11-05 23:29:21 +0900

    시작 시, 화면 위치 설정

--- src/screen/GameFrame.java
+++ src/screen/GameFrame.java
@@ -13,11 +13,12 @@ public class GameFrame extends JFrame {
     public GameFrame() {
         setTitle("타이핑 게임");
         setSize(900, 800);
+        setLocation(500, 100);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
         Container c = getContentPane();
-        // c.add(new StartMenuPanel());
-        c.add(new GameSplitPane());
+        c.add(new StartMenuPanel());
+        // c.add(new GameSplitPane());
         c.add(new GameToolBar(),BorderLayout.NORTH);
 
         // c.add(new screen.GameToolBar(), BorderLayout.NORTH);
